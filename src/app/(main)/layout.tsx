b00d8d14f45f1e5e58613ba809a537748eb56df7
clockk <kig0421@gmail.com>
2025-05-16 22:37:07 +0900

    feat: og tag 및 meta data 수정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,13 +5,25 @@ import Head from 'next/head';
 import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata = {
-  title: 'Periodic Tools',
-  description: 'Handle the elements with ease',
-  icons: {
-    icon: '/favicon.svg',
+  title: "Periodic Tools",
+  description: "주기율표 기반 원소 정보와 wt%, at% 단위 변환 도구를 제공하는 Periodic Tools. 화합물 조합을 쉽게 도와주는 과학 계산 도구입니다.",
+  openGraph: {
+    title: "Periodic Tools",
+    description: "원소 정보 확인부터 wt% ↔ at% 단위 변환까지. Periodic Tools로 화합물 조합을 쉽고 정확하게!",
+    url: "https://periodic-tools.c1ock.com",
+    siteName: "Periodic Tools",
+    images: [
+      {
+        url: "og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Periodic Tools 대표 이미지",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
   },
 };
-
 type LaoutProps = {
   children: React.ReactNode;
 }
